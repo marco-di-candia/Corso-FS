6955449fbfef5a4bef3ace53757f23a58800f8fa
marco-di-candia <mdicandia89@gmail.com>
2024-02-24 13:26:27 +0100

    ES-Settimanale-S5/L5

--- UNIT-2/UNIT-2-Settimana-1/UNIT-1-Progetto Settimanale-S5-L5/assets/js/script.js
+++ UNIT-2/UNIT-2-Settimana-1/UNIT-1-Progetto Settimanale-S5-L5/assets/js/script.js
@@ -1,15 +1,25 @@
-let header = document.querySelector("header")
-let colorWhite = document.getElementById("start")
-let colorButton = document.getElementById("start-1")
-let headertop = header.offsetHeight;
-let colorWhitebottom = colorWhite.offsetTop + colorWhite.offsetHeight;
-const cambiacolore = () =>{
-    if (window.scrollY > colorWhitebottom - headertop) {
+let header = document.querySelector("header");
+let colorWhite = document.getElementById("start");
+let colorButton = document.getElementById("start-1");
+const cambiacolore = () => {
+    if (window.scrollY > 496) {
         header.classList.add("coloreNuovo-header")
         colorButton.classList.add("coloreNuovo-button")
-    }else{
+    } else {
         header.classList.remove("coloreNuovo-header")
         colorButton.classList.remove("coloreNuovo-button")
     }
 }
-window.addEventListener("scroll" , cambiacolore)
+window.addEventListener("scroll", cambiacolore)
+
+// set interval M
+let svg = document.querySelectorAll("[stroke-linecap]");
+const M = () => {
+    let random = Math.floor(Math.random() * svg.length)
+    if (svg[random].style.opacity === "0") {
+        svg[random].style.opacity = "0"
+    } else {
+        svg[random].style.opacity = "0"
+    }
+}
+setInterval(M, 50)
